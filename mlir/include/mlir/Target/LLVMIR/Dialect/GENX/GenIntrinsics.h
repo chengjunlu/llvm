551cbf4e64c3585ccb8de3e8092a33158004e17a
Lu, Chengjun <chengjun.lu@intel.com>
2024-02-29 15:04:52 +0000

    Update the GenISA library.

--- mlir/include/mlir/Target/LLVMIR/Dialect/GENX/GenIntrinsics.h
+++ mlir/include/mlir/Target/LLVMIR/Dialect/GENX/GenIntrinsics.h
@@ -1,36 +1,40 @@
-//===- GenIntrinsics.h - IGC GenISAIntrinsics interface ---------*- C++ -*-===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-//
-// This provides IGC GenISAIntrinsic interfaces for GENX dialect
-//
-//===----------------------------------------------------------------------===//
+/*========================== begin_copyright_notice ============================
 
+Copyright (C) 2017-2021 Intel Corporation
+
+SPDX-License-Identifier: MIT
+
+============================= end_copyright_notice ===========================*/
 #pragma once
 
 #include "GenIntrinsicEnum.h"
 
+//#include "common/LLVMWarningsPush.hpp"
 #include "llvm/IR/Function.h"
+//#include "common/LLVMWarningsPop.hpp"
 
 #include <string>
 #include <vector>
 
-namespace llvm {
+namespace llvm
+{
 
-namespace GenISAIntrinsic {
+namespace GenISAIntrinsic
+{
 
 /// Intrinsic::getName(ID) - Return the LLVM name for an intrinsic, such as
 /// "llvm.ppc.altivec.lvx".
-std::string getName(ID id, ArrayRef<Type *> Tys = std::nullopt);
+std::string getName(ID id, ArrayRef<Type*> Tys = std::nullopt);
 
-struct IntrinsicComments {
-  const char *funcDescription;
-  std::vector<const char *> outputs;
-  std::vector<const char *> inputs;
+/// Intrinsic::getType(ID) - Return the function type for an intrinsic.
+///
+FunctionType *getType(LLVMContext &Context, ID id, ArrayRef<Type*> Tys = std::nullopt);
+
+struct IntrinsicComments
+{
+    const char* funcDescription;
+    std::vector<const char*> outputs;
+    std::vector<const char*> inputs;
 };
 
 IntrinsicComments getIntrinsicComments(ID id);
@@ -51,17 +55,16 @@ IntrinsicComments getIntrinsicComments(ID id);
 ///    Type Ts[2]{int2, int2}: to resolve to the first instance.
 ///    Type Ts[2]{int4, int4}: to resolve to the second.
 #if defined(ANDROID) || defined(__linux__)
-__attribute__((visibility("default"))) Function *
-getDeclaration(Module *M, ID id, ArrayRef<Type *> OverloadedTys = std::nullopt);
+__attribute__ ((visibility ("default"))) Function *getDeclaration(Module *M, ID id, ArrayRef<Type*> OverloadedTys = std::nullopt);
 #else
-Function *getDeclaration(Module *M, ID id,
-                         ArrayRef<Type *> OverloadedTys = None);
+Function *getDeclaration(Module *M, ID id, ArrayRef<Type*> OverloadedTys = None);
 #endif
 
-// Override of isIntrinsic method defined in Function.h
-inline const char *getGenIntrinsicPrefix() { return "llvm.genx."; }
-inline bool isIntrinsic(const Function *CF) {
-  return (CF->getName().starts_with(getGenIntrinsicPrefix()));
+//Override of isIntrinsic method defined in Function.h
+inline const char * getGenIntrinsicPrefix() { return "llvm.genx."; }
+inline bool isIntrinsic(const Function *CF)
+{
+    return (CF->getName().starts_with(getGenIntrinsicPrefix()));
 }
 ID getIntrinsicID(const Function *F, bool useContextWrapper = true);
 
